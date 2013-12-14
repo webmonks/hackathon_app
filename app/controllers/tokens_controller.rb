b91028ed86d1b69b1a12116c025ce5ca3fa313bd
Deploy <deploy@wearemonk.com>
2013-12-14 08:22:58 +0000

    protect tokens

--- app/controllers/tokens_controller.rb
+++ app/controllers/tokens_controller.rb
@@ -2,6 +2,7 @@ class TokensController < ApplicationController
 require 'barby'
 require 'barby/outputter/png_outputter'
 require 'barby/barcode/qr_code'
+  before_filter :check_password
   def index
     @tokens = Token.all
   end
